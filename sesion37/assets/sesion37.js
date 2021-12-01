e888bc28e8e2934c037f1870af8a9b39acc2e5ac
AlexEliasC <aelias@crackthecode.pe>
2021-12-01 16:18:49 -0500

    ejemplos de las clases

---
+++ sesion37/assets/sesion37.js
@@ -0,0 +1,50 @@
+/*
+const listaDeNumeros = [2,3,4,7,11];
+console.log(listaDeNumeros[0]);
+console.log(listaDeNumeros[4]);
+*/
+/*
+let numerosImpares = [1,2,3,5,7];
+cantidad = numerosImpares.length;
+console.log("primera forma:")
+for (i=0; cantidad>i; i++){
+  console.log(numerosImpares[i])
+}
+
+console.log("segundo forma:")
+for(const valor of numerosImpares){
+  console.log(valor);
+}
+*/
+
+// Practicando arreglos
+/*
+const listaDeNumeros = [2,3,5,7,11];
+listaDeNumeros[2]= 13;
+console.log(listaDeNumeros[2]);
+
+
+//cambiando valores
+listaDeNumeros=[];
+listaDeNumeros[0]=100;
+listaDeNumeros[1]=101;
+listaDeNumeros[2]=-10;
+listaDeNumeros[3]=20;
+listaDeNumeros[4]=-5;
+console.log(listaDeNumeros[2])
+*/
+/*
+const mixedArray = [3, "hola mundo", 10.5];
+cantidad = mixedArray.length;
+for(i = 0; cantidad>i; i++){
+console.log(mixedArray[i]);
+}
+*/
+let questions = [
+  ['¿Cúal es la capital de Peru?', 'Lima'],
+  ['¿Cuantos colores tiene el arcoiris?', 7],
+  ['¿Cúal es la moneda de Peru?','Sol'],
+];
+
+console.log(questions[0][0]);
+console.log(questions[0][1]);
